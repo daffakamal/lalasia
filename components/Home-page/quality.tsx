4edd7c5107445c08d404d3277035aabf010c23a6
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-07 21:11:26 +0700

    update quality section

--- components/Home-page/quality.tsx
+++ components/Home-page/quality.tsx
@@ -5,12 +5,12 @@ function Quality () {
         <div className="w-full h-full grid content-center px-16 xl:px-32 2xl:px-52 pb-32 bg-white">
             <div className="grid space-y-3">
                 <h3 className="font-eudoxusBold text-yellow text-[18px]">Our Quality</h3>
-                <div className="flex flex-row gap-[308px]">
-                    <div className="grid space-y-5">
+                <div className="flex flex-row gap-32">
+                    <div className="grid space-y-5 w-full">
                         <h2 className="font-eudoxusBold text-black text-[44px] leading-[57px]">Crafted by talented and high quality material</h2>
                         <p className="font-eudoxusRegular text-[18px] text-grey py-5">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non purus parturient.</p>
                         <Link href="/">
-                            <a className="font-eudoxusBold flex justify-center w-[170px] cursor-pointer text-white bg-green hover:bg-cstmdarkerbrown hover:scale-100 duration-300 py-2 2xl:py-3 rounded text-[18px]">
+                            <a className="font-eudoxusBold flex justify-center items-center w-[170px] cursor-pointer text-white bg-green hover:bg-green2 duration-300 py-2 2xl:py-1 rounded text-[18px]">
                                 Learn More
                             </a>
                         </Link>
@@ -19,7 +19,26 @@ function Quality () {
                             className="pt-5 w-[595px]"
                         />
                     </div>
-                {/* buat sisi kanan */}
+                    <div className="grid w-full space-y-5">
+                        <div className="grid grid-cols-3">
+                            <div>
+                                <p className="font-eudoxusBold text-black text-[44px]">20+</p>
+                                <p className="font-eudoxusMedium text-grey text-[18px]">Years Experience</p>
+                            </div>
+                            <div>
+                                <p className="font-eudoxusBold text-black text-[44px]">483</p>
+                                <p className="font-eudoxusMedium text-grey text-[18px]">Happy Client</p>
+                            </div> 
+                            <div>
+                                <p className="font-eudoxusBold text-black text-[44px]">150+</p>
+                                <p className="font-eudoxusMedium text-grey text-[18px]">Project Finished</p>
+                            </div> 
+                        </div>
+                        <img 
+                            src="/images/Home-page/unsplash_376KN_ISplE.svg"
+                            className="pt-5 w-[595px]"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
