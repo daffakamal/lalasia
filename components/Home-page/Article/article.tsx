7b384f837c1f676ed23c1297879d81c800439fc6
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-19 11:32:54 +0700

    update articleCards

--- components/Home-page/Article/article.tsx
+++ components/Home-page/Article/article.tsx
@@ -6,8 +6,8 @@ function Article () {
         <div className="w-full h-full grid content-center px-16 xl:px-32 2xl:px-52 pb-32 bg-white">
             <div className="grid space-y-3">
                 <h3 className="font-eudoxusBold text-yellow text-[18px]">Articles</h3>
-                <div className="flex flex-col xl:flex-row gap-28">
-                    <div className="grid space-y-10 w-full my-auto">
+                <div className="flex items-start flex-col xl:flex-row gap-28">
+                    <div className="grid content-start space-y-10 w-full">
                         <h2 className="font-eudoxusBold text-black text-[44px] leading-[57px]">The best furniture comes from Lalasia</h2>
                         <p className="font-eudoxusRegular text-[18px] text-grey">Pellentesque etiam blandit in tincidunt at donec.</p>
                         <div className="pt-5">
--- components/Home-page/Article/articleCards.tsx
+++ components/Home-page/Article/articleCards.tsx
@@ -1,23 +1,72 @@
+const styleMain="flex space-x-10 my-auto"
+const styleImg="w-[260px] pointer-events-none"
+const styleContent="flex flex-col self-center space-y-5 my-auto"
+const styleH5="font-eudoxusMedium text-[16px] text-grey"
+const styleH4="font-eudoxusBold text-[24px] text-black"
+const styleP="font-eudoxusMedium text-[18px] text-grey"
+const styleWriterContainer="flex items-center space-x-5"
+const styleImgWriter="w-[28px] pointer-events-none"
+const styleWriter="font-eudoxusBold text-[14px] text-black"
+const styleDate="font-eudoxusMedium text-[14px] text-grey pl-14"
+
 export default function Cards () {
     return (
-        <div className="grid space-y-5 w-full my-auto">
-            <div className="flex flex-col">
-                <div className="flex space-x-10">
+        <div className="w-full grid space-y-5">
+            <div className="grid my-auto space-y-10">
+                <div className={styleMain}>
                     <img
                         src="/images/Home-page/Rectangle 20.svg"
-                        className="w-[260px] pointer-events-none"
+                        className={styleImg}
                     />
-                    <div className="flex flex-col space-y-5 my-auto">
-                        <h5 className="font-eudoxusMedium text-[18px] text-grey">Tips and Trick</h5>
-                        <h4 className="font-eudoxusBold text-[24px] text-black">6 ways to give your home minimalistic vibes</h4>
-                        <p className="font-eudoxusMedium text-[18px] text-grey">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum .</p>
-                        <div className="flex items-center space-x-5">
+                    <div className={styleContent}>
+                        <h5 className={styleH5}>Tips and Trick</h5>
+                        <h4 className={styleH4}>6 ways to give your home minimalistic vibes</h4>
+                        <p className={styleP}>Pellentesque etiam blandit in tincidunt at donec. Eget ipsum .</p>
+                        <div className={styleWriterContainer}>
                             <img
                                 src="/images/Home-page/Ellipse 25.svg"
-                                className="w-[28px] pointer-events-none"
+                                className={styleImgWriter}
                             />
-                            <p className="font-eudoxusBold text-[14px] text-black">By Jerremy Jean</p>
-                            <p className="font-eudoxusMedium text-[14px] text-grey pl-14">Friday, 1 April 2022</p>
+                            <p className={styleWriter}>By Jerremy Jean</p>
+                            <p className={styleDate}>Friday, 1 April 2022</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styleMain}>
+                    <img
+                        src="/images/Home-page/Rectangle 20.svg"
+                        className={styleImg}
+                    />
+                    <div className={styleContent}>
+                        <h5 className={styleH5}>Design Inspiration</h5>
+                        <h4 className={styleH4}>How to make your interiors cooler and more stylish</h4>
+                        <p className={styleP}>Pellentesque etiam blandit in tincidunt at donec. Eget ipsum .</p>
+                        <div className={styleWriterContainer}>
+                            <img
+                                src="/images/Home-page/Ellipse 25 (1).svg"
+                                className={styleImgWriter}
+                            />
+                            <p className={styleWriter}>By Michaela Augus</p>
+                            <p className={styleDate}>Friday, 1 April 2022</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styleMain}>
+                    <img
+                        src="/images/Home-page/Rectangle 20.svg"
+                        className={styleImg}
+                    />
+                    <div className={styleContent}>
+                        <h5 className={styleH5}>Tips and Trick</h5>
+                        <h4 className={styleH4}>Elements to add character to your space</h4>
+                        <p className={styleP}>Pellentesque etiam blandit in tincidunt at donec. Eget ipsum .</p>
+                        <div className={styleWriterContainer}>
+                            <img
+                                src="/images/Home-page/Ellipse 25 (2).svg"
+                                className={styleImgWriter}
+                            />
+                            <p className={styleWriter}>By Kim Gurameh</p>
+                            <p className={styleDate}>Friday, 1 April 2022</p>
                         </div>
                     </div>
                 </div>
