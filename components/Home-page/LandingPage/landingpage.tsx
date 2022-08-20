31badb3bca3f8626f54053f5448b63cc10f67dac
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-20 15:41:11 +0700

    update landing page

--- components/Home-page/LandingPage/landingpage.tsx
+++ components/Home-page/LandingPage/landingpage.tsx
@@ -14,8 +14,8 @@ function LandingPage () {
                             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                             </div>
-                            <input type="search" className="font-eudoxusMedium block p-4 pl-10 w-[600px] text-[18px] text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-green3 focus:ring-green3 focus:ring-1" placeholder="Search" />
-                            <button type="submit" className="font-eudoxusMedium text-white absolute right-2.5 bottom-2.5 bg-green hover:bg-green2 font-medium rounded-lg text-[18px] px-4 py-2">Search</button>
+                            <input type="search" className="font-eudoxusMedium block p-4 pl-10 w-[600px] text-[18px] text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-green3 focus:ring-green3 focus:ring-1" placeholder="Search property" />
+                            <button type="submit" className="font-eudoxusMedium text-white absolute right-2.5 bottom-2.5 bg-green hover:bg-green2 font-medium rounded-lg text-[18px] px-4 py-2 pointer-event">Search</button>
                         </div>
                     </form>
                 </div>
