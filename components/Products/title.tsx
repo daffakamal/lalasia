19ff91c632230d0596e224ccad3a0cb89df6bba3
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-30 18:50:58 +0700

    update responsive (font size, padding) on title section

--- components/Products/title.tsx
+++ components/Products/title.tsx
@@ -2,9 +2,9 @@
 function Title () {
     return (
         <div className="w-full h-full bg-white">
-            <div className="grid justify-items-center px-[24px] sm:px-16 xl:px-32 2xl:px-52 pt-52">
+            <div className="grid justify-items-center px-[24px] sm:px-16 xl:px-32 2xl:px-52 pt-28 md:pt-32 lg:pt-52">
                 <div className="grid justify-items-center xl:container xl:mx-auto space-y-5">
-                    <h2 className="font-eudoxusBold text-5xl md:text-[64px] leading-[70px] md:leading-[83px] text-[#151411] text-center">Products</h2>
+                    <h2 className="font-eudoxusBold text-4xl sm:text-5xl md:text-[64px] md:leading-[83px] text-[#151411] text-center">Products</h2>
                     <div className="font-eudoxusRegular text-sm sm:text-base md:text-lg text-grey leading-[25px] sm:leading-[32px] text-center pb-14">
                         <p>We display products based on the latest products we have, if you want to see our old products please enter the name of the item</p>
                     </div>
@@ -16,13 +16,18 @@ function Title () {
                         src="/images/Home-page/Rectangle 38.svg"
                         className="w-full pointer-events-none brightness-90"
                     />
-                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed">
-                        <div className="flex justify-start items-center h-full px-[132px] py-[149px]">
-                            <div className="text-white space-y-5">
-                                <a className="inline-block px-5 py-2 font-eudoxusMedium bg-green text-gray-200 font-medium text-sm leading-snug rounded-3xl"
-                                >Discount</a>
-                                <h2 className="font-eudoxusBold text-4xl">Ramadhan Sale Offer</h2>
-                                <h4 className="font-eudoxusMedium text-xl">Get 40% off for the first transaction on Lalasia</h4>
+                    <div className="w-full h-full absolute top-0 right-0 bottom-0 left-0 overflow-hidden bg-fixed">
+                        <div className="h-full flex justify-start items-center px-[24px] sm:px-16 xl:px-32 2xl:px-[132px] py-[149px] ">
+                            <div className="text-white space-y-2 lg:space-y-5">
+                                <a className="inline-block px-5 py-2 font-eudoxusMedium bg-green text-gray-200 text-xs md:text-sm leading-snug rounded-3xl">
+                                    Discount
+                                </a>
+                                <h2 className="font-eudoxusBold text-[24px] sm:text-[32px] md:text-[36px] lg:text-[44px]">
+                                    Ramadhan Sale Offer
+                                </h2>
+                                <h4 className="font-eudoxusMedium text-xs sm:text-lg md:text-xl">
+                                    Get 40% off for the first transaction on Lalasia
+                                </h4>
                             </div>
                         </div>
                     </div>
