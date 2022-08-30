4357c9ba682146d510093fb9961f7047d4f67b1b
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-30 18:14:58 +0700

    update responsive for totalProduct section

--- components/Products/totalProduct.tsx
+++ components/Products/totalProduct.tsx
@@ -14,11 +14,11 @@ function TotalProduct () {
                                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                 </div>
-                                <input type="search" className="font-eudoxusMedium block p-4 pl-10 w-[600px] text-base md:text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-green3 focus:ring-green3 focus:ring-1" placeholder="Search property" />
-                                <button type="submit" className="font-eudoxusMedium text-white absolute right-2.5 bottom-2.5 bg-green hover:bg-green2 rounded-lg text-base md:text-lg px-4 py-2 pointer-event">Find Now</button>
+                                <input type="search" className="font-eudoxusMedium block p-4 pl-10 w-[300px] sm:w-[390px] md:w-[460px] lg:w-[600px] text-base md:text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-green3 focus:ring-green3 focus:ring-1" placeholder="Search property" />
+                                <button type="submit" className="font-eudoxusMedium text-white absolute right-2.5 bottom-2.5 bg-green hover:bg-green2 rounded-lg text-base md:text-lg px-4 py-2 pointer-event">Search</button>
                             </div>
                         </form>
-                        <button className="h-full flex items-center px-7 py-3 font-eudoxusMedium text-black bg-whitebasic hover:bg-gray-100 rounded-lg text-base md:text-lg cursor-pointer">
+                        <button className="lg:h-full items-center hidden md:flex px-5 lg:px-7 py-3 font-eudoxusMedium text-black bg-whitebasic hover:bg-gray-100 rounded-lg text-base md:text-lg cursor-pointer">
                             <img 
                                 src="/images/Products/filter.svg"
                                 className="w-[25px] pointer-events-none"
@@ -26,16 +26,16 @@ function TotalProduct () {
                             Filter
                         </button>
                     </div>
-                    <div className="w-full grid grid-cols-2 content-center">
+                    <div className="w-full grid md:grid-cols-2 content-center">
                         <div className="flex items-center">
-                            <h2 className="font-eudoxusBold text-black text-[24px] sm:text-[40px] md:text-[44px] pr-5">
+                            <h2 className="font-eudoxusBold text-black text-[24px] sm:text-[32px] md:text-[36px] lg:text-[44px] pr-5">
                                 Total Product
                             </h2>
-                            <div className="flex items-center px-4 py-1 font-eudoxusMedium text-green bg-whitebasic rounded-3xl text-base md:text-lg">
+                            <div className="flex items-center px-4 py-1 font-eudoxusMedium text-green bg-whitebasic rounded-3xl text-xs sm:text-base md:text-lg">
                                 <a>184</a>
                             </div>
                         </div>
-                        <div className="flex justify-end">
+                        <div className="flex justify-end hidden md:flex">
                             <div className="flex items-center">
                                 <Menu as="div" className="relative inline-block text-left">
                                     <div>
