1751b5f3abed0d20cd6365e08c6b87c9db90223b
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-30 09:40:18 +0700

    add headlessui and update sort by button

--- components/Products/totalProduct.tsx
+++ components/Products/totalProduct.tsx
@@ -1,4 +1,6 @@
 import ProductList from "./productList";
+import { Menu, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
 
 function TotalProduct () {
     return (
@@ -34,13 +36,88 @@ function TotalProduct () {
                             </div>
                         </div>
                         <div className="flex justify-end">
-                            <button className="flex items-center px-4 py-3 font-eudoxusMedium text-black bg-whitebasic hover:bg-gray-100 rounded-lg text-base md:text-lg cursor-pointer">
-                                <img 
-                                    src="/images/Products/sort.svg"
-                                    className="w-[25px] pointer-events-none"
-                                />
-                                Sort By
-                            </button>
+                            <div className="flex items-center">
+                                <Menu as="div" className="relative inline-block text-left">
+                                    <div>
+                                        <Menu.Button className="inline-flex w-full justify-center rounded-md bg-whitebasic px-4 py-2 font-eudoxusMedium text-base md:text-lg text-black hover:bg-gray-100">
+                                            <img 
+                                            src="/images/Products/sort.svg"
+                                            className="w-[25px] pointer-events-none"
+                                            />
+                                            Sort By
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <div className="px-1 py-1">
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                    <button
+                                                        className={`${
+                                                        active ? 'bg-green text-white text-base md:text-lg' : 'text-gray-900 text-base md:text-lg'
+                                                        } group flex w-full items-center rounded-md px-3 py-2`}
+                                                    >
+                                                        Price low to high
+                                                    </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                    <button
+                                                        className={`${
+                                                        active ? 'bg-green text-white text-base md:text-lg' : 'text-gray-900 text-base md:text-lg'
+                                                        } group flex w-full items-center rounded-md px-3 py-2`}
+                                                    >
+                                                        Price high to low
+                                                    </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                    <button
+                                                        className={`${
+                                                        active ? 'bg-green text-white text-base md:text-lg' : 'text-gray-900 text-base md:text-lg'
+                                                        } group flex w-full items-center rounded-md px-3 py-2`}
+                                                    >
+                                                        Chair
+                                                    </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                    <button
+                                                        className={`${
+                                                        active ? 'bg-green text-white text-base md:text-lg' : 'text-gray-900 text-base md:text-lg'
+                                                        } group flex w-full items-center rounded-md px-3 py-2`}
+                                                    >
+                                                        Table
+                                                    </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                    <button
+                                                        className={`${
+                                                        active ? 'bg-green text-white text-base md:text-lg' : 'text-gray-900 text-base md:text-lg'
+                                                        } group flex w-full items-center rounded-md px-3 py-2`}
+                                                    >
+                                                        Cupboard
+                                                    </button>
+                                                    )}
+                                                </Menu.Item>
+                                            </div>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
                         </div>
                     </div>
                     <ProductList />
