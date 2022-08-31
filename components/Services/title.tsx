0a1842fdece42f70cfcbfb78bccaf456dafcea79
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-08-31 21:07:12 +0700

    complete the title section

--- components/Services/title.tsx
+++ components/Services/title.tsx
@@ -1,3 +1,6 @@
+const styleNumber="font-eudoxusBold text-green text-4xl sm:text-5xl md:text-[64px]"
+const styleSubTitle="font-eudoxusBold text-black text-sm sm:text-lg md:text-2xl lg:text-[24px] sm:pt-2 md:pt-5"
+const styleDescription="font-eudoxusMedium text-grey text-xs sm:text-sm md:text-lg lg:text-lg"
 
 function Title () {
     return (
@@ -12,8 +15,76 @@ function Title () {
                     </div>
                     <img 
                         src="/images/Services/Rectangle 24.svg"
-                        className="w-full pointer-events-none pb-10"
+                        className="w-full pointer-events-none"
                     />
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-14 gap-y-16 md:gap-y-20 pt-20">
+                        <div className="grid space-y-3">
+                            <p className={styleNumber}>
+                                01
+                            </p>
+                            <h3 className={styleSubTitle}>
+                                Furniture
+                            </h3>
+                            <p className={styleDescription}>
+                                At the ultimate smart home, you're met with technology before you even step through the front door. 
+                            </p>
+                        </div>
+                        <div className="grid space-y-3">
+                            <p className={styleNumber}>
+                                02
+                            </p>
+                            <h3 className={styleSubTitle}>
+                                Home Decoration
+                            </h3>
+                            <p className={styleDescription}>
+                                Create a calming summer escape with our luxurious home accessories for the balmy evenings.
+                            </p>
+                        </div>
+                        <div className="grid space-y-3">
+                            <p className={styleNumber}>
+                                03
+                            </p>
+                            <h3 className={styleSubTitle}>
+                                Kitchen Cabinet
+                            </h3>
+                            <p className={styleDescription}>
+                                Introducing the modular kitchen cabinet system. Start with our huge selection of base and wall cabinets. 
+                            </p>
+                        </div>
+                        <div className="grid space-y-3">
+                            <p className={styleNumber}>
+                                04
+                            </p>
+                            <h3 className={styleSubTitle}>
+                                Interior Design
+                            </h3>
+                            <p className={styleDescription}>
+                                Innovative products often feature innovative designs that play with the patterns we are familiar.
+                            </p>
+                        </div>
+                        <div className="grid space-y-3">
+                            <p className={styleNumber}>
+                                05
+                            </p>
+                            <h3 className={styleSubTitle}>
+                                Exterior Design
+                            </h3>
+                            <p className={styleDescription}>
+                                These stylish and resilient products provide up-to-date options for roofing, siding, decking, and outdoor spaces.
+                            </p>
+                        </div>
+                        <div className="grid space-y-3">
+                            <p className={styleNumber}>
+                                06
+                            </p>
+                            <h3 className={styleSubTitle}>
+                                Custom Furniture
+                            </h3>
+                            <p className={styleDescription}>
+                                With Quality Materials and Modern Designs, we have Designs for all Tastes. we bring you World Class Designs.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
