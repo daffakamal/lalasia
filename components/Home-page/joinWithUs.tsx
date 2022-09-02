8889427c8dd7e44a10ef33f420a54e3f471165a6
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-02 18:55:23 +0700

    fix joinWithUs section

--- components/Home-page/joinWithUs.tsx
+++ components/Home-page/joinWithUs.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
 
 function JoinWithUs () {
     return (
-        <div className="w-full h-full grid content-center px-16 xl:px-32 2xl:px-52 pt-28 bg-white">
-            <div className="grid grid-cols-4 xl:container xl:mx-auto">
+        <div className="w-full h-full grid content-center px-16 xl:px-32 2xl:px-52 md:pt-28 bg-white">
+            <div className="grid lg:grid-cols-4 xl:container xl:mx-auto">
                 <h2 className="col-span-3 font-eudoxusBold text-black text-[24px] sm:text-[40px] md:text-[44px] leading-[31px] sm:leading-[57px]">Join with us to get special discount</h2>
-                <div className="grid justify-items-end">
+                <div className="grid justify-items-start lg:justify-items-end pt-6 lg:pt-0">
                     <Link href="#!">
-                        <a className="font-eudoxusBold flex justify-center items-center cursor-pointer text-white bg-green hover:bg-green2 duration-300 px-10 rounded text-sm sm:text-base md:text-lg">
+                        <a className="font-eudoxusBold flex justify-center items-center cursor-pointer text-white bg-green hover:bg-green2 duration-300 px-4 sm:px-8 py-2 gap-x-3 rounded text-sm sm:text-base md:text-lg">
                             Learn More
+                            <ArrowLongRightIcon className="w-6 sm:w-9" />
                         </a>
                     </Link>
                 </div>
