5856c728fb18bf6e0dd3f014464a2eff2057987c
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-07 23:31:20 +0700

    update styling

--- components/About/ourMission.tsx
+++ components/About/ourMission.tsx
@@ -2,6 +2,11 @@
 function OurMission () {
     const styleNumber="font-eudoxusBold text-black text-[22px] sm:text-[40px] md:text-[44px]"
     const styleWord="font-eudoxusMedium text-grey text-sm sm:text-base md:text-lg"
+    const styleContent="flex gap-x-10"
+    const styleImg="w-[62px] pointer-events-none"
+    const styleSubContent="grid space-y-3"
+    const styleTitle="font-eudoxusBold text-sm sm:text-base md:text-lg xl:text-2xl text-black leading-[25px] sm:leading-[32px]"
+    const styleDescription="font-eudoxusRegular text-sm sm:text-base md:text-lg text-grey leading-[25px] sm:leading-[32px]"
 
     return (
         <div className="w-full h-full grid content-center px-[24px] sm:px-16 xl:px-32 2xl:px-52 py-48 bg-white">
@@ -33,44 +38,44 @@ function OurMission () {
                     </div>
                     <div>
                         <div className="grid space-y-10 justify-items-start">
-                            <div className="flex gap-x-10">
+                            <div className={styleContent}>
                                 <img 
                                     src="/images/About/Group 15.svg"
-                                    className="w-[62px] pointer-events-none"
+                                    className={styleImg}
                                 />
-                                <div className="grid space-y-3">
-                                    <h3 className="font-eudoxusBold text-sm sm:text-base md:text-lg xl:text-2xl text-black leading-[25px] sm:leading-[32px]">
+                                <div className={styleSubContent}>
+                                    <h3 className={styleTitle}>
                                         24/7 Supports
                                     </h3>
-                                    <p className="font-eudoxusRegular text-sm sm:text-base md:text-lg text-grey leading-[25px] sm:leading-[32px]">
+                                    <p className={styleDescription}>
                                         24/7 support means a support service that is provided 24 hours a day and 7 days a week
                                     </p>
                                 </div>
                             </div>
-                            <div className="flex gap-x-10">
+                            <div className={styleContent}>
                                 <img 
                                     src="/images/About/Group 16.svg"
-                                    className="w-[62px] pointer-events-none"
+                                    className={styleImg}
                                 />
-                                <div className="grid space-y-3">
-                                    <h3 className="font-eudoxusBold text-sm sm:text-base md:text-lg xl:text-2xl text-black leading-[25px] sm:leading-[32px]">
+                                <div className={styleSubContent}>
+                                    <h3 className={styleTitle}>
                                         Free Consultation
                                     </h3>
-                                    <p className="font-eudoxusRegular text-sm sm:text-base md:text-lg text-grey leading-[25px] sm:leading-[32px]">
+                                    <p className={styleDescription}>
                                         A free consultation is a one-on-one interaction or conversation given freely to share one's thoughts and discuss possible
                                     </p>
                                 </div>
                             </div>
-                            <div className="flex gap-x-10">
+                            <div className={styleContent}>
                                 <img 
                                     src="/images/About/Group 17.svg"
-                                    className="w-[62px] pointer-events-none"
+                                    className={styleImg}
                                 />
-                                <div className="grid space-y-3">
-                                    <h3 className="font-eudoxusBold text-sm sm:text-base md:text-lg xl:text-2xl text-black leading-[25px] sm:leading-[32px]">
+                                <div className={styleSubContent}>
+                                    <h3 className={styleTitle}>
                                         Overall Guarantee
                                     </h3>
-                                    <p className="font-eudoxusRegular text-sm sm:text-base md:text-lg text-grey leading-[25px] sm:leading-[32px]">
+                                    <p className={styleDescription}>
                                     The comprehensive guarantee is required for import, warehousing, transit, processing and specific use
                                     </p>
                                 </div>
