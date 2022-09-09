495dc83feb1aaa6e45be89d500591124e88de1e8
Daffa <daffa.kamal@mail.ugm.ac.id>
2022-09-09 15:36:22 +0700

    add OurTeam section on About page

--- pages/AboutUs/index.tsx
+++ pages/AboutUs/index.tsx
@@ -3,6 +3,7 @@ import { Footer } from '../../components/Layout/Footer';
 import { Navbar } from '../../components/Layout/Navbar';
 import { Title } from '../../components/About/title';
 import { OurMission } from '../../components/About/ourMission';
+import { OurTeam } from '../../components/About/OurTeam/ourTeam';
 
 const About: NextPage = () => {
     return (
