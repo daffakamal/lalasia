211c593301fe4d9c7563a6ead45459a70f32550e
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-25 16:00:07 +0700

    fix: update responsiveness on Article page

--- components/Home-page/Article/articleCards.tsx
+++ components/Home-page/Article/articleCards.tsx
@@ -1,12 +1,12 @@
 
-export default function Cards () {
-    const styleMain="flex space-x-10 my-auto"
-    const styleImg="w-[260px] pointer-events-none"
-    const styleContent="flex flex-col self-center space-y-5 my-auto"
-    const styleH5="font-eudoxusMedium text-[12px] sm:text-[14px] md:text-[16px] text-grey"
+export default function ArticleCards () {
+    const styleMain="grid md:flex md:space-x-10 my-auto max-w-sm md:max-w-full"
+    const styleImg="w-full md:w-[260px] pointer-events-none"
+    const styleContent="flex flex-col self-center space-y-3 md:space-y-5 my-auto"
+    const styleH5="font-eudoxusMedium text-[12px] sm:text-[14px] md:text-[16px] text-grey pt-5 md:pt-0"
     const styleH4="font-eudoxusBold text-[16px] sm:text-[22px] md:text-[24px] text-black"
     const styleP="font-eudoxusMedium text-sm sm:text-base md:text-lg text-grey"
-    const styleWriterContainer="w-full flex items-center space-x-5"
+    const styleWriterContainer="w-full flex items-center space-x-3 md:space-x-5"
     const styleImgWriter="w-[18px] sm:w-[28px] pointer-events-none"
     const styleWriter="w-full lg:w-2/3 2xl:w-1/2 font-eudoxusBold text-[12px] md:text-[14px] text-black"
     const styleDate="invisible lg:visible font-eudoxusMedium text-[14px] text-grey"
