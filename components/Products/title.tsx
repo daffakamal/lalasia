d08c828f850810fda0138452133cd5d9b2c27b4c
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-10-05 11:10:45 +0700

    feat(Products): add aos on title section

--- components/Products/title.tsx
+++ components/Products/title.tsx
@@ -2,8 +2,8 @@
 function Title () {
     return (
         <div className="w-full h-full bg-white">
-            <div className="grid justify-items-center px-[24px] sm:px-16 xl:px-32 2xl:px-52 pt-28 md:pt-32 lg:pt-52">
-                <div className="grid justify-items-center xl:container xl:mx-auto space-y-5">
+            <div className="grid justify-items-center px-[24px] sm:px-16 xl:px-32 2xl:px-52 pt-28 md:pt-32 lg:pt-52" data-aos="fade-up" data-aos-duration="2000">
+                <div className="grid justify-items-center xl:container xl:mx-auto space-y-5" data-aos="fade-zoom-in" data-aos-duration="2000">
                     <h2 className="font-eudoxusBold text-4xl sm:text-5xl md:text-[64px] md:leading-[83px] text-[#151411] text-center">
                         Products
                     </h2>
@@ -13,13 +13,13 @@ function Title () {
                 </div>
             </div>
             <div className="w-full 3xl:container 3xl:mx-auto pb-14">
-                <div className="relative overflow-hidden bg-no-repeat bg-cover">
+                <div className="relative overflow-hidden bg-no-repeat bg-cover" data-aos="fade-zoom-in" data-aos-duration="1500" data-aos-easing="ease-in" data-aos-delay="100">
                     <img 
                         src="/images/Home-page/Rectangle 38.svg"
                         className="w-full pointer-events-none brightness-90"
                     />
                     <div className="w-full h-full absolute top-0 right-0 bottom-0 left-0 overflow-hidden bg-fixed">
-                        <div className="h-full flex justify-start items-center px-[24px] sm:px-16 xl:px-32 2xl:px-[132px] py-[149px] ">
+                        <div className="h-full flex justify-start items-center px-[24px] sm:px-16 xl:px-32 2xl:px-[132px] py-[149px]">
                             <div className="text-white space-y-2 lg:space-y-5">
                                 <a className="inline-block px-5 py-2 font-eudoxusMedium bg-green text-gray-200 text-xs md:text-sm leading-snug rounded-3xl">
                                     Discount
