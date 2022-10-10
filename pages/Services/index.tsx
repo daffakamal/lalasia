6b939013bc87fd70e354b1d5bdb32765a2e5ac09
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-10-10 20:30:01 +0700

    feat(Services): add next/head

--- pages/Services/index.tsx
+++ pages/Services/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { Footer } from '../../components/Layout/Footer';
 import { Navbar } from '../../components/Layout/Navbar';
 import { Title } from '../../components/Services/title';
@@ -8,6 +9,10 @@ import { Interested } from '../../components/Services/interested';
 const Services: NextPage = () => {
     return (
       <>
+        <Head>
+          <title>Services</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Navbar />
         <Title />
         <Portfolio />
