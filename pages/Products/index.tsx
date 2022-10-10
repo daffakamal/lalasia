067351399feecd3ac1017e057890d45b9efc7941
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-10-10 20:28:06 +0700

    feat(About): add next/head

--- pages/Products/index.tsx
+++ pages/Products/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { Footer } from '../../components/Layout/Footer';
 import { Navbar } from '../../components/Layout/Navbar';
 import { Title } from '../../components/Products/title';
@@ -7,6 +8,10 @@ import { TotalProduct } from '../../components/Products/totalProduct';
 const Products: NextPage = () => {
     return (
       <>
+        <Head>
+          <title>Products</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Navbar />
         <Title />
         <TotalProduct />
