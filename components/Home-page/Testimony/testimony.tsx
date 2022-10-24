69d1fbc1965fe879ff5aac7822481f7c731e5787
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-10-24 13:58:24 +0700

    fix(Home-page): tidy up code

--- components/Home-page/Testimony/testimony.tsx
+++ components/Home-page/Testimony/testimony.tsx
@@ -2,7 +2,11 @@
 function Testimony () {
     return (
         <div className="w-full h-full grid justify-items-center px-16 xl:px-32 2xl:px-52 pb-32 bg-white">
-            <div className="grid justify-items-center space-y-5" data-aos="fade-up" data-aos-duration="1500">
+            <div 
+                className="grid justify-items-center space-y-5" 
+                data-aos="fade-up" 
+                data-aos-duration="1500"
+            >
                 <h3 className="font-eudoxusBold text-yellow text-sm sm:text-base md:text-lg">
                     Testimonials
                 </h3>
@@ -12,7 +16,11 @@ function Testimony () {
                 <div className="font-eudoxusRegular text-sm sm:text-base md:text-lg leading-[25px] sm:leading-[32px] text-grey pb-10">
                     <p>Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non purus parturient.</p>
                 </div>
-                <div className="rounded bg-whitebasic p-10 space-y-10 max-w-2xl" data-aos="fade-zoom-in" data-aos-duration="1500" data-aos-easing="ease-in">
+                <div className="rounded bg-whitebasic p-10 space-y-10 max-w-2xl" 
+                    data-aos="fade-zoom-in" 
+                    data-aos-duration="1500" 
+                    data-aos-easing="ease-in"
+                >
                     <p className="font-eudoxusRegular text-sm sm:text-base md:text-lgleading-[25px] sm:leading-[32px] text-grey">
                         Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris non. 
                     </p>
@@ -20,6 +28,7 @@ function Testimony () {
                         <img
                             src="/images/Home-page/Ellipse 1.png"
                             className="w-[28px] sm:w-[40px] md:w-[50px] pointer-events-none"
+                            alt="Person picture"
                         />
                         <p className="font-eudoxusBold text-black text-sm sm:text-base md:text-[20px]">
                             Janne Cooper
