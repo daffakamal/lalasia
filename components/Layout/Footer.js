4eb7368beac456eeff6d917d15f82d803492c883
defz-07 <93387102+wannabegooddevv@users.noreply.github.com>
2023-11-14 23:27:06 +0700

    style(Products): formatting code for Products component

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,99 +1,79 @@
 import Link from "next/link";
 
 function Footer() {
-    const styleContent = "font-eudoxusMedium cursor-pointer text-black hover:text-green text-sm sm:text-base md:text-lg"
+  const styleContent =
+    "font-eudoxusMedium cursor-pointer text-black hover:text-green text-sm sm:text-base md:text-lg";
 
-    return (
-        <div className="w-full h-full grid content-center px-7 sm:px-16 xl:px-32 2xl:px-52 py-28 bg-white">
-            <div className="w-full xl:container xl:mx-auto border border-greyBorder"></div>
-            <div className="grid grid-rows-4 lg:grid-rows-1 grid-cols-none lg:grid-cols-5 xl:container xl:mx-auto pt-20">
-                <div className="col-span-3 w-full lg:w-3/4">
-                    <Link href="#/">
-                        <img   
-                            src="/images/Home-page/Frame 1.svg"
-                            className="w-[99px] md:w-[130px] pointer-events"
-                            alt="Lalasia logo"
-                        />
-                    </Link>
-                    <p className="font-eudoxusMedium text-sm sm:text-base md:text-lg leading-[25px] sm:leading-[32px] text-black pt-5 md:pt-10">
-                        Lalasia is digital agency that help you make better experience iaculis cras in.
-                    </p>
-                </div>
-                <div className="grid content-start col-span-1 sm:col-span-3 sm:flex sm:flex-wrap row-span-3 lg:row-span-1 justify-end lg:col-span-2 justify-between sm:pt-20 lg:pt-0">
-                    <div className="grid space-y-5">
-                        <p className="font-eudoxusBold cursor-pointer text-black text-sm sm:text-base md:text-lg">
-                            Product
-                        </p>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                New Arrivals
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Best Selling
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Home Decor
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Kitchen Set
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="grid space-y-5 pt-10 sm:pt-0">
-                        <p className="font-eudoxusBold cursor-pointer text-black text-sm sm:text-base md:text-lg">
-                            Services
-                        </p>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Catalog
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Blog
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                FAQ
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Pricing
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="grid content-start space-y-5 pt-10 sm:pt-0">
-                        <p className="font-eudoxusBold cursor-pointer text-black text-sm sm:text-base md:text-lg">
-                            Follow Us
-                        </p>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Facebook
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Instagram
-                            </a>
-                        </Link>
-                        <Link href="#!">
-                            <a className={styleContent}>
-                                Twitter
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="w-full h-full grid content-center px-7 sm:px-16 xl:px-32 2xl:px-52 py-28 bg-white">
+      <div className="w-full xl:container xl:mx-auto border border-greyBorder"></div>
+      <div className="grid grid-rows-4 lg:grid-rows-1 grid-cols-none lg:grid-cols-5 xl:container xl:mx-auto pt-20">
+        <div className="col-span-3 w-full lg:w-3/4">
+          <Link href="#/">
+            <img
+              src="/images/Home-page/Frame 1.svg"
+              className="w-[99px] md:w-[130px] pointer-events"
+              alt="Lalasia logo"
+            />
+          </Link>
+          <p className="font-eudoxusMedium text-sm sm:text-base md:text-lg leading-[25px] sm:leading-[32px] text-black pt-5 md:pt-10">
+            Lalasia is digital agency that help you make better experience
+            iaculis cras in.
+          </p>
         </div>
-    );
+        <div className="grid content-start col-span-1 sm:col-span-3 sm:flex sm:flex-wrap row-span-3 lg:row-span-1 justify-end lg:col-span-2 justify-between sm:pt-20 lg:pt-0">
+          <div className="grid space-y-5">
+            <p className="font-eudoxusBold cursor-pointer text-black text-sm sm:text-base md:text-lg">
+              Product
+            </p>
+            <Link href="#!">
+              <a className={styleContent}>New Arrivals</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Best Selling</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Home Decor</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Kitchen Set</a>
+            </Link>
+          </div>
+          <div className="grid space-y-5 pt-10 sm:pt-0">
+            <p className="font-eudoxusBold cursor-pointer text-black text-sm sm:text-base md:text-lg">
+              Services
+            </p>
+            <Link href="#!">
+              <a className={styleContent}>Catalog</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Blog</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>FAQ</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Pricing</a>
+            </Link>
+          </div>
+          <div className="grid content-start space-y-5 pt-10 sm:pt-0">
+            <p className="font-eudoxusBold cursor-pointer text-black text-sm sm:text-base md:text-lg">
+              Follow Us
+            </p>
+            <Link href="#!">
+              <a className={styleContent}>Facebook</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Instagram</a>
+            </Link>
+            <Link href="#!">
+              <a className={styleContent}>Twitter</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { Footer };
--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -3,172 +3,151 @@ import Link from "next/link";
 import { Transition } from "@headlessui/react";
 
 function Navbar() {
-	const [isOpen, setIsOpen] = useState(false);
-    const ref=React.createRef();
-    const styleTextNav="font-eudoxusMedium text-sm sm:text-base md:text-lg cursor-pointer text-black hover:text-green px-3 py-2"
-	
-	return (
-		<div>
-			<nav 
-				className="w-full fixed bg-white px-7 sm:px-16 xl:px-32 2xl:px-52 border-t-transparent border-l-transparent border-r-transparent border-2 border-[#F3F3F3] z-50" 
-				data-aos="zoom-out-down" 
-				data-aos-duration="1500"
-			>
-				<div className="w-full xl:container xl:mx-auto">
-					<div className="w-full flex items-center h-16 xl:h-20">
-						<div className="w-full flex items-center justify-between">
-							<div className="flex justify-center items-center">
-								<Link href="/">
-									<img
-										loading="lazy"
-										src="/images/Home-page/Frame 1.svg"
-										className="w-[99px] md:w-[120px] lg:w-[130px] cursor-pointer"
-										alt="Lalasia logo"
-									/>
-								</Link>
-							</div>
-							<div className="hidden lg:block">
-								<div className="flex space-x-2 lg:space-x-6 2xl:space-x-11">
-									<Link href="/">
-                                        <a className={styleTextNav}>
-                                        	Home
-                                        </a>
-									</Link>
-									<Link href="/Products">
-                                        <a className={styleTextNav}>
-                                        	Product
-                                        </a>
-									</Link>
-									<Link href="/Services">
-                                        <a className={styleTextNav}>
-                                        	Services
-                                        </a>
-									</Link>
-									<Link href="/Article">
-                                        <a className={styleTextNav}>
-                                        	Article
-                                        </a>
-									</Link>
-									<Link href="/About">
-                                        <a className={styleTextNav}>
-                                        	About Us
-                                        </a>
-									</Link>
-								</div>
-							</div>
-							<div className="flex justify-center items-center space-x-5">
-								<Link href="#!">
-									<img
-										src="/images/Home-page/bag-2.svg"
-										className="hidden lg:block w-[30px] cursor-pointer"
-										alt="Shopping cart"
-									/>
-								</Link>
-								<Link href="/Profile">
-									<img
-										src="/images/Home-page/user.svg"
-										className="hidden lg:block w-[30px] cursor-pointer"
-										alt="Profile"
-									/>
-								</Link>
-							</div>
-						</div>
-						<div className="flex lg:hidden">
-							<button
-								onClick={() => setIsOpen(!isOpen)}
-								type="button"
-								className="bg-white inline-flex items-center justify-center rounded-md text-black"
-								aria-controls="mobile-menu"
-								aria-expanded="false"
-							>
-								<span className="sr-only">Open main menu</span>
-								{!isOpen ? (
-									<svg
-										className="block h-[25px] w-[25px]"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M4 6h16M4 12h16M4 18h16"
-										/>
-									</svg>
-								) : (
-									<svg
-										className="block h-[25px] w-[25px]"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M6 18L18 6M6 6l12 12"
-										/>
-									</svg>
-								)}
-							</button>
-						</div>
-					</div>
-				</div>
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = React.createRef();
+  const styleTextNav =
+    "font-eudoxusMedium text-sm sm:text-base md:text-lg cursor-pointer text-black hover:text-green px-3 py-2";
 
-				<Transition
-					show={isOpen}
-					enter="transition ease-out duration-100 transform"
-					enterFrom="opacity-0 scale-95"
-					enterTo="opacity-100 scale-100"
-					leave="transition ease-in duration-75 transform"
-					leaveFrom="opacity-100 scale-100"
-					leaveTo="opacity-0 scale-95"
-				>
-						<div className="lg:hidden" id="mobile-menu">
-							<div className="grid grid-cols-4">
-								<div className="col-end-5 col-span-1">
-									<div
-										ref={ref}
-										className="grid pt-2 pb-3 space-y-5 text-right font-eudoxusMedium text-sm sm:text-base md:text-lg cursor-pointer text-black hover:text-green py-2"
-									>
-										<Link href="/">
-											<a>
-												Home
-											</a>
-										</Link>
-										<Link href="/Products">
-											<a>
-												Product
-											</a>
-										</Link>
-										<Link href="/Services">
-											<a>
-												Services
-											</a>
-										</Link>
-										<Link href="/Article">
-											<a>
-												Article
-											</a>
-										</Link>
-										<Link href="/">
-											<a>
-												About Us
-											</a>
-										</Link>
-									</div>
-								</div>
-							</div>
-						</div>
-				</Transition>
-			</nav>
-		</div>
-	);
+  return (
+    <nav
+      className="w-full fixed bg-white px-7 sm:px-16 xl:px-32 2xl:px-52 border-t-transparent border-l-transparent border-r-transparent border-2 border-[#F3F3F3] z-50"
+      data-aos="zoom-out-down"
+      data-aos-duration="1500"
+    >
+      <div className="w-full xl:container xl:mx-auto">
+        <div className="w-full flex items-center h-16 xl:h-20">
+          <div className="w-full flex items-center justify-between">
+            <div className="flex justify-center items-center">
+              <Link href="/">
+                <img
+                  loading="lazy"
+                  src="/images/Home-page/Frame 1.svg"
+                  className="w-[99px] md:w-[120px] lg:w-[130px] cursor-pointer"
+                  alt="Lalasia logo"
+                />
+              </Link>
+            </div>
+            <div className="hidden lg:block">
+              <div className="flex space-x-2 lg:space-x-6 2xl:space-x-11">
+                <Link href="/">
+                  <a className={styleTextNav}>Home</a>
+                </Link>
+                <Link href="/Products">
+                  <a className={styleTextNav}>Product</a>
+                </Link>
+                <Link href="/Services">
+                  <a className={styleTextNav}>Services</a>
+                </Link>
+                <Link href="/Article">
+                  <a className={styleTextNav}>Article</a>
+                </Link>
+                <Link href="/About">
+                  <a className={styleTextNav}>About Us</a>
+                </Link>
+              </div>
+            </div>
+            <div className="flex justify-center items-center space-x-5">
+              <Link href="#!">
+                <img
+                  src="/images/Home-page/bag-2.svg"
+                  className="hidden lg:block w-[30px] cursor-pointer"
+                  alt="Shopping cart"
+                />
+              </Link>
+              <Link href="/Auth">
+                <img
+                  src="/images/Home-page/user.svg"
+                  className="hidden lg:block w-[30px] cursor-pointer"
+                  alt="Profile"
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-white inline-flex items-center justify-center rounded-md text-black"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-[25px] w-[25px]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-[25px] w-[25px]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <div className="lg:hidden" id="mobile-menu">
+          <div className="grid grid-cols-4">
+            <div className="col-end-5 col-span-1">
+              <div
+                ref={ref}
+                className="grid pt-2 pb-3 space-y-5 text-right font-eudoxusMedium text-sm sm:text-base md:text-lg cursor-pointer text-black hover:text-green py-2"
+              >
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+                <Link href="/Products">
+                  <a>Product</a>
+                </Link>
+                <Link href="/Services">
+                  <a>Services</a>
+                </Link>
+                <Link href="/Article">
+                  <a>Article</a>
+                </Link>
+                <Link href="/">
+                  <a>About Us</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Transition>
+    </nav>
+  );
 }
 
 export { Navbar };
